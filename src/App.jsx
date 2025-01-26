54844a29b1fd6328ec2ac8f9929da199c3982ebe
Mateo-Rey <jrey3darts@gmail.com>
2025-01-26 16:30:30 -0500

    add 9 more games

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,8 @@ function App() {
       <h1 className='font-bold text-6xl text-black p-4'>FAU Game Night</h1>
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 content-center place-items-center bg-[#14B8A6] p-4'>
        
-        <GameCard image={cs2} name={"Counter-Strike 2"} genre={"FPS"} url={"https://www.counter-strike.net/cs2"}/>
-        <GameCard image={cs2} name={"Counter-Strike 2"} genre={"FPS"} url={"https://www.counter-strike.net/cs2"}/>
-        <GameCard image={cs2} name={"Counter-Strike 2"} genre={"FPS"} url={"https://www.counter-strike.net/cs2"}/>
-        <GameCard image={cs2} name={"Counter-Strike 2"} genre={"FPS"} url={"https://www.counter-strike.net/cs2"}/>
-        <GameCard image={cs2} name={"Counter-Strike 2"} genre={"FPS"} url={"https://www.counter-strike.net/cs2"}/>
-        <GameCard image={cs2} name={"Counter-Strike 2"} genre={"FPS"} url={"https://www.counter-strike.net/cs2"}/>
-        <GameCard image={cs2} name={"Counter-Strike 2"} genre={"FPS"} url={"https://www.counter-strike.net/cs2"}/>
-        <GameCard image={cs2} name={"Counter-Strike 2"} genre={"FPS"} url={"https://www.counter-strike.net/cs2"}/>
-        <GameCard image={cs2} name={"Counter-Strike 2"} genre={"FPS"} url={"https://www.counter-strike.net/cs2"}/>
-        <GameCard image={cs2} name={"Counter-Strike 2"} genre={"FPS"} url={"https://www.counter-strike.net/cs2"}/>
+        <GameCard image={cs2} name={"Counter-Strike 2"} genre={"FPS"} url={"https://www.counter-strike.net/cs2"} appId={"730"}/>
+      
         
       </div>
     </div>
