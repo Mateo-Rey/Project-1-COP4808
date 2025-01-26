9158add4bf587dbd7ebf2d8bbf658a3363e64991
Mateo-Rey <jrey3darts@gmail.com>
2025-01-26 16:55:35 -0500

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import minecraft from "./assets/minecraft.jpg"
 function App() {
   return (
     <div className="min-h-screen">
-      <h1 className='font-bold text-6xl text-black p-4'>FAU Game Night</h1>
+      <h1 className='font-bold text-6xl text-white p-4'><p className=' header-shadow'>FAU Game Night</p></h1>
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 content-center place-items-center bg-[#14B8A6] p-4'>
        
         <GameCard image={cs2} name={"Counter-Strike 2"} genre={"FPS"} url={"https://www.counter-strike.net/cs2"}/>
